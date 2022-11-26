e02f49e8c21c6f16bd14a547f5065fc51e43cd57
Le Nguyen Ngoc Han <80307347+lenguyenngochan@users.noreply.github.com>
2022-11-26 12:56:51 +0700

    Add files via upload

---
+++ insert_center.sql
@@ -0,0 +1,2 @@
+INSERT INTO `btl`.`Center` (`center_id`, `center_name`, `center_address`)
+VALUE (1, 'Trung tâm anh ngữ ABC', '268, Lý Thường Kiệt, quận 10, thành phố Hồ Chí Minh');
---
+++ insert_class.sql
@@ -0,0 +1,38 @@
+INSERT INTO `btl`.`Class` (`class_id`, `class_name`, `course_id`, `schedule_id`, `created_at`)
+VALUE (1, 'B01', 1, 1, '2022-11-01 00:00:01');
+
+INSERT INTO `btl`.`Class` (`class_id`, `class_name`, `course_id`, `schedule_id`, `created_at`)
+VALUE (2, 'B02', 1, 2, '2022-11-01 00:00:01');
+
+INSERT INTO `btl`.`Class` (`class_id`, `class_name`, `course_id`, `schedule_id`, `created_at`)
+VALUE (3, 'B03', 1, 3, '2022-11-01 00:00:01');
+
+INSERT INTO `btl`.`Class` (`class_id`, `class_name`, `course_id`, `schedule_id`, `created_at`)
+VALUE (4, 'B04', 1, 4, '2022-11-01 00:00:01');
+
+INSERT INTO `btl`.`Class` (`class_id`, `class_name`, `course_id`, `schedule_id`, `created_at`)
+VALUE (5, 'B05', 1, 5, '2022-11-01 00:00:01');
+
+INSERT INTO `btl`.`Class` (`class_id`, `class_name`, `course_id`, `schedule_id`, `created_at`)
+VALUE (6, 'T01', 2, 6, '2022-11-01 00:00:01');
+
+INSERT INTO `btl`.`Class` (`class_id`, `class_name`, `course_id`, `schedule_id`, `created_at`)
+VALUE (7, 'T02', 2, 7, '2022-11-01 00:00:01');
+
+INSERT INTO `btl`.`Class` (`class_id`, `class_name`, `course_id`, `schedule_id`, `created_at`)
+VALUE (8, 'T03', 2, 8, '2022-11-01 00:00:01');
+
+INSERT INTO `btl`.`Class` (`class_id`, `class_name`, `course_id`, `schedule_id`, `created_at`)
+VALUE (9, 'P01', 3, 9, '2022-11-01 00:00:01');
+
+INSERT INTO `btl`.`Class` (`class_id`, `class_name`, `course_id`, `schedule_id`, `created_at`)
+VALUE (10, 'P02', 3, 10, '2022-11-01 00:00:01');
+
+INSERT INTO `btl`.`Class` (`class_id`, `class_name`, `course_id`, `schedule_id`, `created_at`)
+VALUE (11, 'I01', 4, 11, '2022-11-01 00:00:01');
+
+INSERT INTO `btl`.`Class` (`class_id`, `class_name`, `course_id`, `schedule_id`, `created_at`)
+VALUE (12, 'I02', 4, 12, '2022-11-01 00:00:01');
+
+INSERT INTO `btl`.`Class` (`class_id`, `class_name`, `course_id`, `schedule_id`, `created_at`)
+VALUE (13, 'I03', 4, 13, '2022-11-01 00:00:01');
