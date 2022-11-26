206f28e33b56ef747498b084fd911c9ecd3f1e2a
Le Nguyen Ngoc Han <80307347+lenguyenngochan@users.noreply.github.com>
2022-11-26 12:59:01 +0700

    Add files via upload

---
+++ insert_course.sql
@@ -0,0 +1,51 @@
+INSERT INTO `btl`.`Course` (`course_id`, `course_name`, `course_code`, `start_date`, `end_date`, `description`, `programme_id`, `fee`, `created_at`)
+VALUE (
+1,
+'Khóa học anh văn căn bản',
+'Basic_English',
+'2022-12-01',
+'2023-02-01',
+'Học những kiến thức anh văn cơ bản; rèn luyện các kỹ năng: nghe, nói, đọc, viết',
+1,
+1000000,
+'2022-11-01 00:00:01'
+);
+
+INSERT INTO `btl`.`Course` (`course_id`, `course_name`, `course_code`, `start_date`, `end_date`, `description`, `programme_id`, `fee`, `created_at`)
+VALUE (
+2,
+'Khóa luyện thi TOEIC',
+'TOEIC',
+'2022-12-01',
+'2023-04-01',
+'Học những kiến thức cần thiết để thi TOEIC',
+2,
+2000000,
+'2022-11-01 00:00:01'
+);
+
+INSERT INTO `btl`.`Course` (`course_id`, `course_name`, `course_code`, `start_date`, `end_date`, `description`, `programme_id`, `fee`, `created_at`)
+VALUE (
+3,
+'Khóa học pre_IELTS',
+'pre_IELTS',
+'2023-01-01',
+'2023-09-01',
+'Học những kiến thức cơ bản cho khóa luyện thi IELTS',
+3,
+4000000,
+'2022-11-01 00:00:01'
+);
+
+INSERT INTO `btl`.`Course` (`course_id`, `course_name`, `course_code`, `start_date`, `end_date`, `description`, `programme_id`, `fee`, `created_at`)
+VALUE (
+4,
+'Khóa luyện thi IELTS',
+'IELTS',
+'2023-10-01',
+'2024-10-01',
+'Học những kiến thức cần thiết để thi IELTS',
+4,
+7000000,
+'2022-11-01 00:00:01'
+);
