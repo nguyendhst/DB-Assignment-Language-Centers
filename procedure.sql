0cc390fb3d8b8144af6b08de08333d0a0b1f7e29
Le Thanh Khai <khailt.tt@topica.edu.vn>
2022-12-12 11:00:56 +0700

    procedure

--- procedure.sql
+++ procedure.sql
@@ -1,36 +1,40 @@
-
--- Thu tuc goi thong tin nhan vien o co so 1 & 2
+DROP PROCEDURE IF EXISTS emp_infor;
 DELIMITER $$
-CREATE PROCEDURE csdl.emp_infor()
+CREATE PROCEDURE `emp_infor`()
 BEGIN
 	SELECT p.lname, p.fname, p.address, ep.salary, o.center_id
 	FROM person p
 	JOIN employee ep ON p.person_id = ep.person_id
 	JOIN officer o ON ep.employee_id = o.employee_id WHERE o.center_id IN (2,1)
     ORDER BY salary ASC;
-END; $$
+END$$
 DELIMITER ;
-CALL emp_infor();
+CALL emp_infor;
 
--- ==============================================
--- Thu tuc goi thong tin hoc sinh da dong hoc phi
+
+DROP PROCEDURE IF EXISTS student_payment;
 DELIMITER $$
-CREATE PROCEDURE student_payment()
+CREATE DEFINER=`root`@`localhost` PROCEDURE `student_payment`()
 BEGIN
+DROP VIEW IF EXISTS `payment`;
+CREATE VIEW `payment` AS
 	SELECT p.lname, 
 		   p.fname, 
-		   re.course_fee, 
-		   re.discount,
 		   re.total,
-		   re.paid, re.dept, 
+		   re.paid, re.in_debt, 
 		   pe.lname as Emp_Lname, 
 		   pe.fname Emp_Fname
 	FROM student st
 	JOIN person p ON p.person_id = st.person_id
-	JOIN receipt re ON st.student_id = re.student_id
-	JOIN employee em ON re.employee_id = em.employee_id
+	JOIN invoice re ON st.student_id = re.student_id
+	JOIN employee em ON re.officer_id = em.employee_id
 	JOIN person pe ON em.person_id = pe.person_id
-	ORDER BY dept DESC;
-END; $$
+	ORDER BY re.in_debt DESC;
+    
+	SELECT 
+		(SELECT COUNT(*) FROM payment) AS Num_of_student_have_paid,
+        (SELECT COUNT(*) FROM student) AS Num_of_student
+    From dual;
+END$$
 DELIMITER ;
-CALL student_payment();
+CALL student_payment;
